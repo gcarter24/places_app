8ef523468ed1aca2d99f82179200cecd309a874b
George <gchriscarter@gmail.com>
2021-03-29 14:42:33 -0400

    add restful routes and actions with happy/sad paths

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/places" => "places#index"
+    post "/places" => "places#create"
+    get "/places/:id" => "places#show"
+    patch "/places/:id" => "places#update"
+    delete "/places/:id" => "places#destroy"
+  end
 end
